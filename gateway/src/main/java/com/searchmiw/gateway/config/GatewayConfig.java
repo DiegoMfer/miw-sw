a825edd29b8741628294fb99383d2ae6c65e3fca
DiegoMfer <diegomartinfnz@gmail.com>
2025-04-17 17:26:50 +0200

    change

--- gateway/src/main/java/com/searchmiw/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/com/searchmiw/gateway/config/GatewayConfig.java
@@ -18,23 +18,5 @@ public class GatewayConfig {
     @Value("${USER_SERVICE_URL:http://user:8084}")
     private String userServiceUrl;
 
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                // Auth Service Routes
-                .route("auth-service", r -> r.path("/auth/**")
-                        .uri(authServiceUrl))
-                
-                // User Service Routes
-                .route("user-service", r -> r.path("/api/users/**")
-                        .uri(userServiceUrl))
-                
-                // Data Aggregator GraphQL Routes
-                .route("data-aggregator-graphql", r -> r.path("/graphql/**")
-                        .uri(dataAggregatorUrl))
-                .route("data-aggregator-graphiql", r -> r.path("/graphiql")
-                        .uri(dataAggregatorUrl))
-                
-                .build();
-    }
+    
 }
