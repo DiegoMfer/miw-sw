f9a882d18f3ca353a77d01641537453c3fb1886e
DiegoMfer <diegomartinfnz@gmail.com>
2025-04-29 12:36:03 +0200

    auth-service template

---
+++ auth-service/src/test/java/com/searchmiw/auth/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.searchmiw.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
