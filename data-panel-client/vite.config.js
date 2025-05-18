7cbc380782fe2d2569f0725a69ab3391622aa1be
DiegoMfer <diegomartinfnz@gmail.com>
2025-05-18 14:30:55 +0000

    certificates for both clients

--- data-panel-client/vite.config.js
+++ data-panel-client/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    // port: 5173, // default, can be omitted if not customized
+    port: 3001, // default, can be omitted if not customized
     open: true, // Automatically open the app in the default browser
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'certs/data-panel-client.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certs/data-panel-client.crt')),
+    },
   },
 })
--- search-miw-client/vite.config.js
+++ search-miw-client/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import fs from 'fs';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    // port: 5173, // default, can be omitted if not customized
+    port: 3000,
     open: true, // Automatically open the app in the default browser
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'certs/search-client.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certs/search-client.crt')),
+    },
   },
 })
